f71d411456177a58b65d9e8f912c02c7dc16c776
Tiphaine Phe-Neau <tiph.pheneau@gmail.com>
2020-02-19 16:39:36 +0100

    Add ecb forex download function

---
+++ src/data/ecb/get_data.py
@@ -0,0 +1,17 @@
+import os
+
+from src.data.ecb import source_config
+from src.data.utils import download_file_from_url, unzip_file, rename_file
+
+
+def get_forex():
+    """Gets latest Forex to Euros data."""
+    print(">> Downloading the latest ECB Forex data...")
+    url = source_config.forex_data_url['latest']
+    output_zip = source_config.forex_data_files['latest']['zip']
+    download_file_from_url(url, output_zip)
+    unzip_file(output_zip, source_config.ecb_raw)
+    rename_file(os.path.join(source_config.ecb_raw, 'eurofxref-hist.csv'),
+                source_config.forex_data_files['latest']['raw'])
+
+
