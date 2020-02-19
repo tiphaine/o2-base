3bd5d7e76792b8e2cb50e59154ba7eff095cb391
Tiphaine Phe-Neau <tiph.pheneau@gmail.com>
2020-02-19 11:25:38 +0100

    Add conversion data and remove unused file

---
+++ src/data/config.py
@@ -0,0 +1,14 @@
+month_abr_fr_to_number = {
+    'janv': 1,
+    'févr': 2,
+    'mars': 3,
+    'avr': 4,
+    'mai': 5,
+    'juin': 6,
+    'juil': 7,
+    'août': 8,
+    'sept': 9,
+    'oct': 10,
+    'nov': 11,
+    'déc': 12,
+}
